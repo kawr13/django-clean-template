887d2b1d802cc89fdad830a66156b2ccbf6d3576
kawr13 <62603910+kawr13@users.noreply.github.com>
2024-04-30 14:56:31 +0500

    Update urls.py

--- src/config/urls.py
+++ src/config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include('start.urls', namespace='start')),
+
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('schema/swagger-ui/', SpectacularSwaggerView .as_view(url_name='schema'), name='swagger-ui'),
